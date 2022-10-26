225e9265d0eec210475af9c7bd36e77dca3fd8ae
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-10-26 22:24:09 +0200

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  integrations: [],
+  site: "https://AstragoDEShare.github.io",
+  base: "/3-basic-responsive-CSS-SCSS-Layouts",
 });
